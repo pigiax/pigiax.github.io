ca5cc6dc83e298717b53d9526cd4ab35c162ddf6
Marco Pigliapoco <marco.pigliapoco@doit.it>
2025-07-15 21:42:55 +0200

    fix: fixed location and updated countdown after end

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const updateCountdown = () => {
     const now = new Date();
     const diff = weddingDate - now;
     const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-    countdownEl.textContent = `Mancano ${days} giorni!`;
+    if (days < 0)
+        countdownEl.textContent = `Grazie di essere stati con noi!`;
+    else
+        countdownEl.textContent = `Mancano ${days} giorni!`;
 };
 updateCountdown();
 setInterval(updateCountdown, 1000 * 60 * 60);
